a04608caedb992e0d3408a2c2f812560b096c51d
mc-Jackie <xieq@mchz.com.cn>
2018-02-12 23:20:52 +0800

    实现模板渲染

--- app/src/components/intelligentMenu/JMenu.js
+++ app/src/components/intelligentMenu/JMenu.js
@@ -1,70 +1,75 @@
 (function(window, document, $, _) {
   var TEMPLATE =
-    ' <div id="JMenu_<%= uuid %>" class="menu-container">\
+    '<div id="JMenu_<%= uuid %>" class="menu-container">\
             <ul>\
-                <% _.each(menu, function(item, idx){\
-                <li data-id="<%= uuid + idx %>" class="menu-active">\
-                    <% _.each(item,function(o){\
-                        <a href="<%= o.href %>"><%= o.name %></a>\
-                    }) %>\
+                <% _.each(menu, function(item, idx){ %>\
+                <li data-id="<%= uuid + idx %>">\
+                    <% _.each(item,function(o){ %>\
+                        <a href="<%= o.href || \'javascript:void(0);\' %>"><%= o.name %></a>\
+                    <% }); %>\
                 </li>\
-                }); %>\
+                <% }); %>\
             </ul>\
             <div id="JMenu_sub_<%= uuid %>" class="sub-menu-wrap none">\
-                <% _.each(children, function(arr, index){\
+                <% _.each(children, function(arr, index){ %>\
                 <div class="sub-menu none" id="<%= uuid + index %>">\
-                    <% _.each(arr,function(o){\
+                    <% _.each(arr,function(child){ %>\
                     <dl>\
                         <dt>\
-                            <a href="#">\
-                                <span>二级菜单1</span>\
+                            <a href="<%= child.href || \'javascript:void(0);\' %>"> \
+                                <span><%= child.name %></span>\
                                 <i class="iconfont icon-qiehuanqiyou"></i>\
                             </a>\
                         </dt>\
                         <dd>\
-                            <a href="#"> 三级菜单 </a>\
+                            <% _.each(child.children, function(grandChild){ %>\
+                            <a href="<%= grandChild.href || \'javascript:void(0);\' %>"> <%= grandChild.name %> </a>\
+                            <% }); %>\
                         </dd>\
                     </dl>\
+                    <% }); %>\
                 </div>\
-                }); %>\
+                <% }); %>\
             </div>\
         </div>';
 
-    var OPTS = {
-        el: '',
-    };
-
-    var tree = [{
-        titles: [{ name: '', href='' }, { name: '', href='' }],
-        children: [{
-            name: '',
-            href: '',
-            children: [{ name: '', href='' }, { name: '', href='' }]
-        }, {
-            name: '',
-            href: '',
-            children: [{ name: '', href='' }, { name: '', href='' }]
-        }]
-    }];
-    
-    // var menu = [[{ name: '', href='' }, { name: '', href='' }], [{ name: '', href='' }, { name: '', href='' }]];
-    // var children = [[{
-    //     name: '',
-    //     href: '',
-    //     children: [{ name: '', href='' }, { name: '', href='' }]
-    // }, {
-    //     name: '',
-    //     href: '',
-    //     children: [{ name: '', href='' }, { name: '', href='' }]
-    // }], [{
-    //     name: '',
-    //     href: '',
-    //     children: [{ name: '', href='' }, { name: '', href='' }]
-    // }, {
-    //     name: '',
-    //     href: '',
-    //     children: [{ name: '', href='' }, { name: '', href='' }]
-    // }]];
+  /**
+   * el: jquery选择器
+   * tree: [{
+   *     titles: [{
+   *         name: '',
+   *         href : ''
+   *     }, {
+   *         name: '',
+   *         href : ''
+   *     },...],
+   *     children: [{
+   *         name: '',
+   *         href: '',
+   *         children: [{
+   *             name: '',
+   *             href : ''
+   *         }, {
+   *             name: '',
+   *             href : ''
+   *         },...]
+   *     }, {
+   *         name: '',
+   *         href: '',
+   *         children: [{
+   *             name: '',
+   *             href : ''
+   *         }, {
+   *             name: '',
+   *             href : ''
+   *         },...]
+   *     },...]
+   * },...]
+   */
+  var OPTS = {
+    el: "",
+    tree: []
+  };
 
   var UTILS = {
     genUUID: function() {
@@ -78,6 +83,19 @@
         }
       );
       return uuid;
+    },
+    handleTree: function(tree) {
+      var menu = [],
+        children = [];
+      if ($.type(tree) !== "array") throw new Error("请传入正确的格式！");
+      tree.forEach(function(node, idx) {
+        menu.push(node.titles);
+        children.push(node.children);
+      });
+      return {
+        menu: menu,
+        children: children
+      };
     }
   };
 
@@ -89,18 +107,37 @@
   JMenu.prototype = {
     constructor: JMenu,
     version: "1.0.0",
-    init: function (config) {
-        if (!config || !config.el) throw new Error('请传入一个jquery选择器！');
-        this.el = config.el; 
-        this.uuid = UTILS.genUUID();
-        return this;  
+    init: function(config) {
+      if (!config || !config.el) throw new Error("请传入一个jquery选择器！");
+      this.el = config.el || OPTS.el;
+      this.tree = config.tree || OPTS.tree;
+      this.uuid = UTILS.genUUID();
+      var result = UTILS.handleTree(this.tree);
+      this.computedMenu = result.menu || [];
+      this.computedChildren = result.children || [];
+      return this.render().bindEvent();
     },
-    render: function() {},
-    template: function() {},
-    setData: function () {},
-    getMenu: function () {},
-    getChildren: function () {},
-    handleData: function () {}
+    render: function() {
+      var html = this.template()({
+        uuid: this.uuid,
+        menu: this.computedMenu,
+        children: this.computedChildren
+      });
+      this.getEl().html(html);
+      return this;
+    },
+    getEl: function() {
+      return $(this.el);
+    },
+    template: function() {
+      return _.template(TEMPLATE);
+    },
+    setData: function(tree) {
+      var result = UTILS.handleTree(tree);
+      result.menu && (this.computedMenu = result.menu);
+      result.children && (this.computedChildren = result.children);
+    },
+    bindEvent: function() {}
   };
 
   window.JMenu = JMenu;
