44893117dc456a91bd573b026eaf414dcc048b94
mc-Jackie <xieq@mchz.com.cn>
2018-02-26 15:08:21 +0800

    事件监听

--- app/src/components/intelligentMenu/JMenu.js
+++ app/src/components/intelligentMenu/JMenu.js
@@ -96,6 +96,53 @@
         menu: menu,
         children: children
       };
+    },
+    // 向量是终点坐标减去起点坐标
+    vector: function(a, b) {
+      return {
+        x: b.x - a.x,
+        y: b.y - a.y
+      };
+    },
+    // 向量的叉乘
+    vectorPro: function(v1, v2) {
+      return v1.x * v2.y - v1.y * v2.x;
+    },
+    // 用位运算高效判断符号相同
+    sameSign: function(a, b) {
+      return (a ^ b) >= 0;
+    },
+    // 判断点是否在三角形内
+    isPointInTranjgle: function(p, a, b, c) {
+      var pa = this.vector(p, a);
+      var pb = this.vector(p, b);
+      var pc = this.vector(p, c);
+
+      var t1 = this.vectorPro(pa, pb);
+      var t2 = this.vectorPro(pb, pc);
+      var t3 = this.vectorPro(pc, pa);
+
+      return this.sameSign(t1, t2) && this.sameSign(t2, t3);
+    },
+    // 是否需要延迟
+    needDelay: function(ele, curMouse, prevMouse) {
+      if (!curMouse || !prevMouse) {
+        return;
+      }
+      var offset = ele.offset();
+
+      // 左上点
+      var topleft = {
+        x: offset.left,
+        y: offset.top
+      };
+      // 左下点
+      var leftbottom = {
+        x: offset.left,
+        y: offset.top + ele.height()
+      };
+
+      return this.isPointInTranjgle(curMouse, prevMouse, topleft, leftbottom);
     }
   };
 
@@ -118,7 +165,7 @@
       return this.render().bindEvent();
     },
     render: function() {
-      var html = this.template()({
+      var html = this.template({
         uuid: this.uuid,
         menu: this.computedMenu,
         children: this.computedChildren
@@ -129,15 +176,106 @@
     getEl: function() {
       return $(this.el);
     },
-    template: function() {
-      return _.template(TEMPLATE);
+    template: function(data) {
+      return _.template(TEMPLATE)(data);
     },
     setData: function(tree) {
       var result = UTILS.handleTree(tree);
       result.menu && (this.computedMenu = result.menu);
       result.children && (this.computedChildren = result.children);
+      return this.render().bindEvent();
     },
-    bindEvent: function() {}
+    bindEvent: function() {
+      var mainMenu = $("#JMenu_" + this.uuid);
+      var subMenu = $("#JMenu_sub_" + this.uuid);
+      var activeRow, activeMenu;
+      var timer;
+
+      // 鼠标是否在子菜单里
+      var isMouseInSub = false;
+      subMenu
+        .on("mouseenter", function() {
+          isMouseInSub = true;
+        })
+        .on("mouseleave", function() {
+          isMouseInSub = false;
+        });
+
+      // 记录鼠标的位置
+      var mouseTrack = [];
+      var moveHandler = function(e) {
+        mouseTrack.push({ x: e.pageX, y: e.pageY });
+
+        // 只需要当前点和上一个点
+        if (mouseTrack.length > 3) {
+          mouseTrack.shift();
+        }
+      };
+
+      // 此处只能给整个菜单注册，而非只给一级菜单加，否则timer中的回调执行时，activeRow已为空
+      mainMenu
+        .on("mouseenter", function() {
+          $(document).bind("mousemove", moveHandler);
+        })
+        .on("mouseleave", function() {
+          subMenu.addClass("none");
+
+          if (activeRow) {
+            activeRow.removeClass("menu-active");
+            activeRow = null;
+          }
+          if (activeMenu) {
+            activeMenu.addClass("none");
+            activeMenu = null;
+          }
+          if (timer) {
+            clearTimeout(timer);
+          }
+
+          $(document).unbind("mousemove", moveHandler); // 注意解绑，以免影响其他组件
+        })
+        .on("mouseenter", "li", function(e) {
+          subMenu.removeClass("none");
+
+          if (!activeRow) {
+            _active(e.target);
+            return;
+          }
+          if (timer) {
+            clearTimeout(timer);
+          }
+
+          var curMouse = mouseTrack[mouseTrack.length - 1]; // 鼠标当前坐标
+          var prevMouse = mouseTrack[mouseTrack.length - 2]; // 鼠标上一次坐标
+
+          var delay = UTILS.needDelay(subMenu, curMouse, prevMouse);
+
+          if (delay) {
+            // 加入延迟器，解决斜方移动切换，只能折线移动的问题
+            timer = setTimeout(function() {
+              if (isMouseInSub) return;
+              _switchActive(e.target);
+              timer = null;
+            }, 300);
+            return;
+          }
+
+          _switchActive(e.target);
+        });
+
+      function _active(target) {
+        activeRow = $(target);
+        activeRow.addClass("menu-active");
+        activeMenu = $("#" + activeRow.data("id"));
+        activeMenu.removeClass("none");
+      }
+
+      function _switchActive(target) {
+        activeRow.removeClass("menu-active");
+        activeMenu.addClass("none");
+        _active(target);
+      }
+    }
   };
 
   window.JMenu = JMenu;
