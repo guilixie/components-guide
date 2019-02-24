cc2c3542a4d395449751d37accd1dfa14f3d61bb
guilixie <597634713@qq.com>
2019-02-24 19:01:37 +0800

    Update gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+// 根目录
+var root = gulp.src('app');
+// 启动一个服务
+gulp.task('webserver', function() {
+    root.pipe(webserver({
+        host: '127.0.0.1',
+        port: 8080,
+        livereload: true,
+        directoryListing: true,
+        open: './index.html'
+    }));
+});
+
+gulp.task('default', ['webserver']);
